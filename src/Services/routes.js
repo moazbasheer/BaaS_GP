1e717bfeb4e34686b74dd38f5f1813734a68e719
dudo48 <test>
2022-07-02 15:31:17 +0200

    create route map done

---
+++ src/Services/routes.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const baseUrl = `https://graphhopper.com/api/1/route?key=${process.env.REACT_APP_GRAPHHOPPER_KEY}`
+
+const getRoute = async points => {
+  let url = baseUrl
+  points.forEach(point => url = url.concat(`&point=${point.join(',')}`))
+  const result = await axios.get(url)
+  return result.data
+}
+
+export default { getRoute }
